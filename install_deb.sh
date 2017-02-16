02915abe43079a80fedbb6680d70229a619bdf7b
hadyRashwan <h2rashwan@gmail.com>
2017-02-16 09:11:58 +0200

    adding random and curl debs in R adding sql_mode='' which prevented sql insertion

--- install_deb.sh
+++ install_deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install all the debs with mysql & apache 
 MYSQL_ROOT_PASSWORD=hello
-apt-get update && apt-get upgrade -y && apt-get install -y apache2 wget git  r-base libmysqlclient-dev r-cran-ggplot2 r-cran-plyr r-cran-reshape2 libcairo2-dev curl npm php php-pear php-mysql php-curl libapache2-mod-php php-mcrypt php-json php-xml php-dom
+apt-get update && apt-get upgrade -y && apt-get install -y apache2 wget git  r-base libmysqlclient-dev r-cran-ggplot2 r-cran-plyr r-cran-reshape2 libcairo2-dev curl npm php php-pear php-mysql php-curl libapache2-mod-php php-mcrypt php-json php-xml php-dom apt libcurl4-openssl-dev
 
 
 ### APACHE CONFIG
@@ -13,6 +13,6 @@ cat /etc/apache2/apache2.conf | awk '/<Directory \/var\/www\/>/,/AllowOverride N
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
 apt-get install -y mysql-server
-service mysql start && mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE concerto"
+service mysql start && mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE concerto" && mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "SET GLOBAL sql_mode='' ;"
 PHP_DATETIMEZONE=${PHP_DATETIMEZONE:-Africa/Cairo} &&  echo "date.timezone=\"$PHP_DATETIMEZONE\"" >> /etc/php/7.0/cli/php.ini
 
